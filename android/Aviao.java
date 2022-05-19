dc47758ea077832a289ebb6327e96c4d3cd523a9
matheus1000 <matheus100039@gmail.com>
2022-05-18 21:02:46 -0400

    inclusão de arquivos java do modulo de android

---
+++ android/Aviao.java
@@ -0,0 +1,14 @@
+class Aviao{
+
+    String modelo = "aviao";
+    String identificador;
+    boolean motor;
+    float altura = 23;
+
+    public void imprimaOk(){
+        System.out.println("Ok");
+    }
+    public String getModelo(){
+        return this.modelo;
+    }
+}
---
+++ android/Main.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String[] args){
+        Aviao latam = new Aviao();
+        String model = latam.getModelo();
+        System.out.println(model);
+    }
+}

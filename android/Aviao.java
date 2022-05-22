5e7713dda2d3e388eecf8247fb4150152adbdb53
matheus1000 <matheus100039@gmail.com>
2022-05-22 13:57:44 -0400

    evolução na tarefa 1

--- android/Aviao.java
+++ android/Aviao.java
@@ -1,14 +1,64 @@
 class Aviao{
 
-    String modelo = "aviao";
+    String modelo;
     String identificador;
     boolean motor;
-    float altura = 23;
+    float altura;
 
-    public void imprimaOk(){
-        System.out.println("Ok");
-    }
     public String getModelo(){
         return this.modelo;
+    }
+
+    public void setModelo(String newModel){
+        this.modelo = newModel;
+    }
+
+    public String getIdentificador(){
+        return this.identificador;
+    }
+
+    public void setIdentificador(String newIdentificador){
+        this.identificador = newIdentificador;
+    }
+
+    public boolean getMotor(){
+        return this.motor;
+    }
+
+    public void setMotor(boolean value){
+        this.motor = value;
+    }
+
+    public float getAltura(){
+        return this.altura;
+    }
+
+    public void setAltura(float newAltura){
+        this.altura = newAltura;
+    }
+
+    Aviao(String modelo, String identificador){
+        this.modelo = modelo;
+        this.identificador = identificador;
+        setMotor(false);
+        setAltura(0);
+    }
+
+    public void ligarMotor(){
+        setMotor(true);
+        System.out.println("Vruuumm");
+    }
+
+    public void desligarMotor(){
+        setMotor(false);
+    }
+
+    public void imprimeEstadoMotor(){
+        if(getMotor()){
+            System.out.println("Motor ON");
+        }
+        else{
+            System.out.println("Motor OFF");
+        }
     }
 }
--- android/Main.java
+++ android/Main.java
@@ -1,7 +1,33 @@
 class Main{
     public static void main(String[] args){
+
+        /*
         Aviao latam = new Aviao();
+        
+        latam.setModelo("monomotor");
         String model = latam.getModelo();
         System.out.println(model);
+
+        latam.setIdentificador("embraer");
+        String ident = latam.getIdentificador();
+        System.out.println(ident);
+
+        latam.setMotor(true);
+        boolean valueMotor = latam.getMotor();
+        System.out.println(valueMotor);
+
+        float newAltura = 12;
+        latam.setAltura(newAltura);
+        float valueAltura = latam.getAltura();
+        System.out.println(valueAltura);
+        */
+
+        Aviao gol = new Aviao("gol", "monomotor");
+
+        gol.ligarMotor();
+        gol.imprimeEstadoMotor();
+
+        gol.desligarMotor();
+        gol.imprimeEstadoMotor();
     }
 }
